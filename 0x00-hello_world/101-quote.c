f3f586b3747342a0834d26c2de3904540784c5e4
Joe <yousef1234328@gmail.com>
2023-03-17 14:53:59 -0700

    create 1-last_digit.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
+
+* main - ptints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+
+* followed by a newline, to standard error.
+
+* Return: Always 1
+
+*/
+
+int main()
+
 {
-	fput(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+
+        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); return (1);
+
 }
