64334b9e605025c911b35a472a75d060b970e7aa
Joe-44 <122934591+Joe-44@users.noreply.github.com>
2023-03-18 00:29:45 +0200

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int l;
+	for (l = 97; l < 123; l++)
+	{
+		putchar(l);
+		putchar('\n');
+	}
+}
