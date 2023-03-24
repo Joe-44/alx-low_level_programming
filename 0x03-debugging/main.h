800230c708c2aed183dfe6447c3860b7b5127c3c
Joe-44 <122934591+Joe-44@users.noreply.github.com>
2023-03-24 05:15:49 +0200

    Update and rename holberton.h to main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN.H
 
 #include <stdio.h>
 
@@ -9,4 +9,3 @@ int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* HOLBERTON_H */
